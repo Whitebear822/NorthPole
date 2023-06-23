38e2227ef988fd3558e7b9b58aa3a9b167259b0f
white bear <arthurkochergin@gmail.com>
2023-06-23 14:08:44 +0700

    first commit

--- Просто.py
+++ Просто.py
@@ -1,6 +1,23 @@
-def recurs(count):
-    print(count)
-    if count == 5:
-        return
-    recurs(count + 1)
-    print(count)
+#Не могу скинуть в git, по скольку он у меня не работает :(
+a = str(input('Введите строку: '))
+list = []
+for bykva in a:
+    list.append(bykva)
+count = len(list)
+list1 = []
+list2 = []
+ccount = count // 2
+for i in range(count):
+    if i < ccount:
+        list1.append(list[i])
+    else:
+        list2.append(list[i])
+list2.reverse()
+count_True = 0
+for i in range(len(list1)):
+    if list1[i] == list2[i]:
+        count_True += 1
+if count_True == len(list1):
+    print(True)
+else:
+    print(False)
