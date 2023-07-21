0184f45aeda4742fc52cfed2e61971bdaafd7cf2
white bear <arthurkochergin@gmail.com>
2023-07-21 15:39:02 +0700

    Django homework

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='main-page'),
+    path('top-sellers', top_sellers, name='top-sellers')
 ]
 
 
