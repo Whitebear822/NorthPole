7b9bbbd3fdda07da7ea5a9e8d4996647df8cecd1
white bear <arthurkochergin@gmail.com>
2023-08-30 02:24:43 +0700

    Admin homework

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
-
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
 # Create your models here.
 
@@ -11,6 +12,28 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Укажите, уместен ли торг')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style = "color: green; font-weight: bold">Сегодня в {}</span', created_time
+            )
+        else:
+            return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Дата редактирования')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style = "color: green; font-weight: bold">Сегодня в {}</span', updated_time
+            )
+        else:
+            return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+
     class Meta:
         db_table = 'advertisements'
     def __str__(self):
